5a9b72f7c444721fa477cd568c32b899d6f10c55
Kerslake <scott.kerslake@uleth.ca>
2018-04-04 11:23:26 -0600

    pushing project files

---
+++ get_tweets.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('TwitterAPIExchange.php');
+
+
+$settings = array(
+    'oauth_access_token' => "2720284888-IH0xwtOOy6kTId31hdrA0LICOw8P4C9MvUQBKMx",
+    'oauth_access_token_secret' => "IKu02oc4MfDdHQcCpkNyIGeqYfI6i7rHlWP8p5aJVVOdm",
+    'consumer_key' => "U4W8y8UShFwtqEFJdkQyna3wA",
+    'consumer_secret' => "h7G5zAPkSqcm2a9qj81OLoVnCCbf35DdOpmvCEOLHGYwGU4Klf"
+
+);
+
+// ?screen_name=sbukkit&count=40&include_rts=true
+// 
+
+// these are the fields that you change for your project
+$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+$getfield = '?screen_name=OhMyCorgi&count=200&include_rts=true';
+$requestMethod = 'GET';
+
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->setGetfield($getfield)
+    ->buildOauth($url, $requestMethod)
+    ->performRequest();
+
+
+?>
+
+
